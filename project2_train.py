9e5e0bca7ccedb543664cdd0b00d8c269040d5d6
cfos3120 <cfos3120@uni.sydney.edu>
2022-10-20 17:48:19 +1100

    bug fixes

--- project2_train.py
+++ project2_train.py
@@ -58,7 +58,7 @@ def train_net(net, trainloader, valloader):
 
         for iter, data in enumerate(trainloader,0):
             # get the inputs
-            inputs, labels = data.to(device)
+            inputs, labels = data
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -103,69 +103,70 @@ def train_net(net, trainloader, valloader):
 # NOTE:
 # Write the train_transform here. We recommend you use
 # Normalization, RandomCrop and any other transform you think is useful.
+if __name__ == '__main__':
 
-train_transform = transforms.Compose([
-    #transforms.RandomCrop(224),
-    transforms.Resize(224),
-    transforms.ToTensor(), 
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+    train_transform = transforms.Compose([
+        #transforms.RandomCrop(224),
+        transforms.Resize(224),
+        transforms.ToTensor(), 
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
 
-####################################
+    ####################################
 
-####################################
-# Define the training dataset and dataloader.
-# You can make some modifications, e.g. batch_size, adding other hyperparameters, etc.
+    ####################################
+    # Define the training dataset and dataloader.
+    # You can make some modifications, e.g. batch_size, adding other hyperparameters, etc.
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-image_path = '/project/MLFluids/5307Project2'
+    image_path = '/project/MLFluids/5307Project2'
+    image_path = r'Z:\PRJ-MLFluids\elec\5307Project2'
+    imageset = ImageFolder(image_path, train_transform)
 
-imageset = ImageFolder(image_path, train_transform)
+    imageset_length = len(imageset)
+    imageset_i = list(range(imageset_length))
 
-imageset_length = len(imageset)
-imageset_i = list(range(imageset_length))
+    split_i = int(0.2 * imageset_length)
 
-split_i = int(torch.floor(0.2 * imageset_length))
+    training_i, validation_i = imageset_i[split_i:], imageset_i[:split_i]
 
-training_i, validation_i = imageset_i[split_i:], imageset_i[:split_i]
-
-train_sampler = torch.utils.data.SubsetRandomSampler(training_i)
-val_sampler = torch.utils.data.SubsetRandomSampler(validation_i)
+    train_sampler = torch.utils.data.SubsetRandomSampler(training_i)
+    val_sampler = torch.utils.data.SubsetRandomSampler(validation_i)
 
 
-# DELETE THIS later when you have the full dataset
-# trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-#                                         download=True, transform=train_transform)
-# valset = torchvision.datasets.CIFAR10(root='./data', train=False,
-#                                         download=True, transform=train_transform)
+    # DELETE THIS later when you have the full dataset
+    # trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    #                                         download=True, transform=train_transform)
+    # valset = torchvision.datasets.CIFAR10(root='./data', train=False,
+    #                                         download=True, transform=train_transform)
 
-trainloader = torch.utils.data.DataLoader(imageset, batch_size=4,
-                                         shuffle=True, num_workers=2, sampler = train_sampler)
-valloader = torch.utils.data.DataLoader(imageset, batch_size=4,
-                                         shuffle=True, num_workers=2, sampler = val_sampler)
-####################################
+    trainloader = torch.utils.data.DataLoader(imageset, batch_size=4,
+                                            shuffle=False, num_workers=2, sampler = train_sampler)
+    valloader = torch.utils.data.DataLoader(imageset, batch_size=4,
+                                            shuffle=False, num_workers=2, sampler = val_sampler)
+    ####################################
 
-# ==================================
-# use cuda if called with '--cuda'.
+    # ==================================
+    # use cuda if called with '--cuda'.
 
-network = Network(
-        image_size=224,
-        patch_size=16,
-        num_layers=12,
-        num_heads=12,
-        hidden_dim=768,
-        mlp_dim=3072
-    )
+    network = Network(
+            image_size=224,
+            patch_size=16,
+            num_layers=12,
+            num_heads=12,
+            hidden_dim=768,
+            mlp_dim=3072
+        )
 
-network.to(device)
-# if args.cuda:
-#     network = network.cuda()
+    network.to(device)
+    # if args.cuda:
+    #     network = network.cuda()
 
-# train and eval your trained network
-# you have to define your own 
-val_acc = train_net(network, trainloader, valloader)
+    # train and eval your trained network
+    # you have to define your own 
+    val_acc = train_net(network, trainloader, valloader)
 
-print("final validation accuracy:", val_acc)
+    print("final validation accuracy:", val_acc)
 
-# ==================================
+    # ==================================
